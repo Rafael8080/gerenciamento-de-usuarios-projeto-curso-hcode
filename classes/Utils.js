219fb386157ce2733cfb683920f4914a4a53d7ff
Rafael Mendes <mafaelrendes8080@gmail.com>
2020-02-24 18:06:56 -0300

    Adicionando explicação sobre metodos estáticos

--- classes/Utils.js
+++ classes/Utils.js
@@ -1,5 +1,6 @@
 class Utils{
 
+	//Um metodo estático, é um metodo onde eu não preciso instaciar. eu chamo direto pelo o nome da classe
 	static dateFormat(date){
 
 		return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes();
